cf01baf5c8796dc92dab4b93d7fa79f9922447e7
Sandbjerg <36782216+Sandbjerg@users.noreply.github.com>
2020-02-18 19:06:42 +0100

    1 change

--- TinaHosted/Program.cs
+++ TinaHosted/Program.cs
@@ -6,7 +6,7 @@ namespace TinaHosted
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World :-)");
         }
     }
 }
