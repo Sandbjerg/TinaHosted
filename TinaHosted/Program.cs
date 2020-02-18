357a6503f7f0417c03c1f0b31ef4467ceaf7781d
Sandbjerg <36782216+Sandbjerg@users.noreply.github.com>
2020-02-18 19:04:50 +0100

    Add project files.

---
+++ TinaHosted/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TinaHosted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
